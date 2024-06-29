604e1ca939a928b3f412c9e443ff4ee157ed017a
El Crisa <elcrisa@gmail.com>
2024-06-29 17:18:14 -0300

    BOB21

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-BOB-0.1";
 const contentToCache = [
     "Build/BUILDS.loader.js",
-    "Build/BUILDS.framework.js.gz",
-    "Build/BUILDS.data.gz",
-    "Build/BUILDS.wasm.gz",
+    "Build/BUILDS.framework.js",
+    "Build/BUILDS.data",
+    "Build/BUILDS.wasm",
     "TemplateData/style.css"
 
 ];
